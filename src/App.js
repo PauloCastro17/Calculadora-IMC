d6eeb69d084e541f1724e17e90cf5c2617bc863b
Paulo Castro <paulo.castro172004@gmail.com>
2022-01-31 20:40:12 -0300

    Calculadora de IMC feito em REACT JS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import './app.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App(){
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+  const [mensage, setMensage] = useState('Esperando dados...');
+
+  function calcularIMC(){
+    const alt = altura /100;
+    const imc = peso/(alt * alt)
+
+
+    if(imc < 18.6){
+      setMensage("Você está abaixo do peso! Seu IMC: " + imc.toFixed(2))
+    }else if(imc >= 18.6 && imc < 24.9){
+      setMensage("Você está no peso ideal! Seu IMC: " + imc.toFixed(2))
+    }else if(imc >= 24.9 && imc < 34.9){
+      setMensage("Você está levemente acima do peso! Seu IMC: " + imc.toFixed(2))
+    }else if(imc >34.9){
+      setMensage("Cuidado Obesidade! Seu IMC: " + imc.toFixed(2))
+    }
+  }
+
+  return(
+    <div className="app">
+      <h1>Calculadora IMC</h1>
+      <span>Vamos calcular seu IMC</span>
+
+      <div className="area-input">
+        <input 
+          type="text"
+          placeholder="Peso em (kg) ex: 90"
+          value={peso}
+          onChange={(e) => setPeso(e.target.value)}
+        />
+        <input 
+          type="text"
+          placeholder="Altura em (cm) ex: 180"
+          value={altura}
+          onChange={(e) => setAltura(e.target.value)}
+        />
+        <button onClick={calcularIMC}>Calcular</button>
+      </div>
+
+      <h2>{mensage}</h2>
     </div>
   );
 }
